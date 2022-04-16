6d795c35e441a590e0cdcdcaa429b4db5d414947
hadeeljamal <hadeeljamal656@gmail.com>
2022-04-16 11:56:41 +0300

    landing

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+
+
+(function($) {
+  "use strict";
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+
+
+
+
+
+  var siteOwlCarousel = function() {
+    $('.testimonial-carousel').owlCarousel({
+      center: true,
+      items: 1,
+      loop: true,
+      margin: 0,
+      autoplay: true,
+      smartSpeed: 1000,
+    });
+  };
+  siteOwlCarousel();
+})(jQuery);
+
+
+AOS.init({
+  easing: 'ease',
+  duration: 1000,
+  once: true
+});
